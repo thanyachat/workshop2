8efb519d3e20cd384fe3e3655cc2e5fa8a549b71
LAPTOP-9HQ6BLP7\Admin <thanyachay.p@ku.th>
2021-01-16 11:48:35 +0700

    Add 6 flie

---
+++ string/slice.py
@@ -0,0 +1,8 @@
+string = "Hello, Word!"
+
+print(string[2:5])  # Qutput : llo
+print(string[:5])  # Output : Hello
+print(string[2:])  # Output : llo,Word!
+print(string[-5:-2])  # output : orl
+print(string[0])  # Output : H
+print(string[-1:])  # Output : !
---
+++ string/string format.py
@@ -0,0 +1,6 @@
+age = 20
+txt = "My name is Priaw, and I am {}"
+
+result = txt.format(age)
+
+print("result:", result)
