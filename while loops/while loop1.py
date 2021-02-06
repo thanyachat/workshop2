19a92a988a6d4c472727d7b57205d54f73a1233f
thanyachay.p@ku.th <thanyachay.p@ku.th>
2021-02-06 22:34:28 +0700

    Add new 5 files.

---
+++ while loops/while loop1.py
@@ -0,0 +1,11 @@
+i = 1
+
+while i < 5:
+    print(i)
+    i += 1
+
+# Output:
+# 1
+# 2
+# 3
+# 4
---
+++ while loops/while loop3.py
@@ -0,0 +1,15 @@
+i = 0
+
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
